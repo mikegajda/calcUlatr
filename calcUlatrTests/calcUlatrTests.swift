fc9ec826610928b87d1bfee74464d24fbb42ed89
Tyrone Hou <tyroneh@bu.edu>
2016-03-20 02:38:32 -0400

    Fixed test parameters

--- calcUlatrTests/calcUlatrTests.swift
+++ calcUlatrTests/calcUlatrTests.swift
@@ -5,12 +5,12 @@ class calcUlatrTests: XCTestCase {
     // MARK: Tests
     
     func testCourseInit() {
-        let potentialCourse = Course(name: "CS111", categories: [Category(name: "tests", weight:10), Category(name: "quizzes", weight:90)])
+        let potentialCourse = Course(name1: "CS111", categories1: [Category(name: "tests", weight:10)!, Category(name: "quizzes", weight:90)!])
         XCTAssertNotNil(potentialCourse)
     }
     
     func testCourseInit3() {
-        let potentialCourse = Course(name: "CS111", categories: [Category(name: "tests", weight:10), Category(name: "tests", weight:10)])
+        let potentialCourse = Course(name1: "CS111", categories1: [Category(name: "tests", weight:10)!, Category(name: "tests", weight:10)!])
         XCTAssertNil(potentialCourse)
     }
     
